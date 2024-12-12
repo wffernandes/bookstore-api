8997a82ebb00868ecb35d653c8b909fad6afad0f
wffernandes <wffernandes01@gmail.com>
2024-12-12 12:45:26 -0300

    Finalizando as classes de dominio

--- src/main/java/com/william/bookstore/domain/Livro.java
+++ src/main/java/com/william/bookstore/domain/Livro.java
@@ -1,13 +1,26 @@
 package com.william.bookstore.domain;
 
+import jakarta.persistence.*;
+
+import java.io.Serial;
+import java.io.Serializable;
 import java.util.Objects;
 
-public class Livro {
+@Entity
+public class Livro implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
     private String titulo;
     private String nome_autor;
     private String texto;
 
+    @ManyToOne
+    @JoinColumn(name = "categoria_id")
     private Categoria categoria;
 
     public Livro() {
