60a41924f103ce7d56fad2d0c786c6b1e930970f
wffernandes <wffernandes01@gmail.com>
2024-12-12 13:12:13 -0300

    repositories implementados

---
+++ src/main/java/com/william/bookstore/repositories/LivroRepository.java
@@ -0,0 +1,9 @@
+package com.william.bookstore.repositories;
+
+import com.william.bookstore.domain.Livro;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface LivroRepository extends JpaRepository<Livro, Integer> {
+}
