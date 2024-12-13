615e2f4369d04d4d2407ad60f53e017382e066c0
wffernandes <wffernandes01@gmail.com>
2024-12-13 09:30:13 -0300

    Instância de forma automática da base de dados

--- src/main/java/com/william/bookstore/BookstoreApplication.java
+++ src/main/java/com/william/bookstore/BookstoreApplication.java
@@ -1,13 +1,37 @@
 package com.william.bookstore;
 
+import com.william.bookstore.domain.Categoria;
+import com.william.bookstore.domain.Livro;
+import com.william.bookstore.repositories.CategoriaRepository;
+import com.william.bookstore.repositories.LivroRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 @SpringBootApplication
-public class BookstoreApplication {
+public class BookstoreApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
+	@Autowired
+	CategoriaRepository categoriaRepository;
+
+	@Autowired
+	LivroRepository livroRepository;
+
+    public static void main(String[] args) {
 		SpringApplication.run(BookstoreApplication.class, args);
-	}
+    }
 
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informática", "Livros de TI");
+		Livro l1 = new Livro(null, "Clean Cod", "Robert Martin", "Lorem ipsum dolor sit amet", cat1);
+
+		cat1.getLivros().addAll(Arrays.asList(l1));
+
+		this.categoriaRepository.saveAll(Arrays.asList(cat1));
+		this.livroRepository.saveAll(Arrays.asList(l1));
+	}
 }
