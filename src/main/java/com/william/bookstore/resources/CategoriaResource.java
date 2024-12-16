70b3f7779bb1689e35956b841e227704c34f304a
wffernandes <wffernandes01@gmail.com>
2024-12-16 10:20:48 -0300

    POST para categoria

--- src/main/java/com/william/bookstore/resources/CategoriaResource.java
+++ src/main/java/com/william/bookstore/resources/CategoriaResource.java
@@ -4,11 +4,10 @@ import com.william.bookstore.domain.Categoria;
 import com.william.bookstore.dtos.CategoriaDTO;
 import com.william.bookstore.service.CategoriaService;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -32,5 +31,12 @@ public class CategoriaResource {
         List<Categoria> list = service.findAll();
         List<CategoriaDTO> listDto = list.stream().map(CategoriaDTO::new).collect(Collectors.toList());
         return ResponseEntity.ok().body(listDto);
+    }
+
+    @PostMapping
+    public ResponseEntity<Categoria> create(@RequestBody Categoria obj) {
+        obj = service.create(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).build();
     }
 }
--- src/main/java/com/william/bookstore/service/CategoriaService.java
+++ src/main/java/com/william/bookstore/service/CategoriaService.java
@@ -25,4 +25,9 @@ public class CategoriaService {
     public List<Categoria> findAll() {
         return repository.findAll();
     }
+
+    public Categoria create(Categoria categoria) {
+        categoria.setId(null);
+        return repository.save(categoria);
+    }
 }
